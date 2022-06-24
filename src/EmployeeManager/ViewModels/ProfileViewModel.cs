9d5f5f5924d70a4f6e0bff79446bce5507788ad9
aczarnick <apz2016@outlook.com>
2022-06-24 00:26:28 -0500

    creating in a view model for displaying the data.

---
+++ src/EmployeeManager/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeManager.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ProfileViewModel(string firstName, string lastName, string title, string bio, string fbProfileLink, string twitterLink, string picture, string department)
+        {
+            Name = $"{firstName} {lastName}";
+            Title = title;
+            Bio = bio;
+            FbProfileLink = fbProfileLink;
+            TwitterLink = twitterLink;
+            Picture = picture;
+            Department = department;
+        }
+
+        public string Name { get; }
+        public string Title { get; }
+        public string Bio { get; }
+        public string FbProfileLink { get; }
+        public string TwitterLink { get; }
+        public string Picture { get; }
+        public string Department { get; }
+
+        
+    }
+}
