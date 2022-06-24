29bb0aefd371fa1a5a238517f8b1b2385b5d4777
aczarnick <apz2016@outlook.com>
2022-06-24 00:32:42 -0500

    Updating index to fetch profile on beginning

--- src/EmployeeManager/Controllers/HomeController.cs
+++ src/EmployeeManager/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManager.Models;
+using EmployeeManager.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,38 @@ namespace EmployeeManager.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly masterContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, masterContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var profiles = from employee in _context.Employees
+                           where employee.Deleted == false
+                           join subDepartment in _context.SubDepartments
+                               on employee.SubDepartmentId equals subDepartment.SubDepartmentId
+                           where subDepartment.Deleted == false
+                           join department in _context.Departments
+                               on subDepartment.DepartmentId equals department.DepartmentId
+                           where department.Deleted == false
+                           select new ProfileViewModel(
+                                employee.FirstName,
+                                employee.LastName,
+                                subDepartment.SubDepartmentName,
+                                employee.Bio,
+                                employee.FbprofileLink,
+                                employee.TwitterProfileLink,
+                                employee.ProfileImage,
+                                department.DepartmentName);
+
+            var result = profiles.AsEnumerable().GroupBy(prof => prof.Department).ToDictionary(group => group.Key, group => group.ToList());
+
+            return View(result);
         }
 
         public IActionResult Privacy()
@@ -27,7 +51,6 @@ namespace EmployeeManager.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
