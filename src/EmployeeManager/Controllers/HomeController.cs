b5d253d6800ba2dd36b9b510921bcc5b006f3d1e
aczarnick <apz2016@outlook.com>
2022-06-23 22:17:28 -0500

    Adding a Crud tab for manging and looking at the objects.

--- src/EmployeeManager/Controllers/HomeController.cs
+++ src/EmployeeManager/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace EmployeeManager.Controllers
         {
             return View();
         }
+        
+        public IActionResult Crud()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
